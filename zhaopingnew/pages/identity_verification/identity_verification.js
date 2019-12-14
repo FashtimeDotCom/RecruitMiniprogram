637365e0c7fdf8feb01dc784dff480c917a924d1
proger <progerchai@qq.com>
2019-12-14 15:27:27 +0800

    优化 身份选择

--- zhaopingnew/pages/identity_verification/identity_verification.js
+++ zhaopingnew/pages/identity_verification/identity_verification.js
@@ -30,78 +30,18 @@ Page({
       })
     }
   },
-  select_hunter: function(e) {
-    var that = this;
-    that.setData({
-      hunter_select: true,
-      interviewer_select: false,
-    });
-  },
-  select_interviewer: function(e) {
-    var that = this;
-    that.setData({
-      hunter_select: false,
-      interviewer_select: true,
-    });
-  },
   go_login: function(e) {
     var identity = app.globalData.identity;
-    console.log(identity);
     if (e.detail.value.radio_identity) {
       app.globalData.identity = e.detail.value.radio_identity
-      console.log('form发生了submit事件，携带数据为：', app.globalData.identity)
       wx.switchTab({
         url: '../main/main',
       })
     } else {
-      console.log('请选择身份')
+      wx.showToast({
+        title: '请先选择身份',
+        icon:'none'
+      });
     }
   },
-  onLoad: function(options) {
-    var that = this;
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        wx.request({
-          url: 'https://www.workoline.com/zhaopin/public/index.php/getsessionkeys',
-          data: {
-            code: res.code
-          },
-          method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-          header: {
-            "content-type": "application/json"
-          }, // 设置请求的 header
-          success: function(res) {
-            console.log("!");
-            console.log(res);
-            var openid = res.data.openid;
-            getApp().globalData.openid = openid;
-            var company_information = that.data.company_information;
-            if (openid) {
-              console.log("openid:" + openid);
-              // 通过openid 获取改用户的uid
-              // wx.request({
-              //   url: 'https://www.workoline.com/zhaopin/public/index.php/authlogin1',
-              //   data: {
-              //     openid: openid,
-              //   },
-              //   method: 'get', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-              //   header: {
-              //     "content-type": "application/json"
-              //   }, // 设置请求的 header
-              //   success: function(res) {
-              //     console.log(res);
-              //     // getApp().d.userId = res.data.arr.id; //后台没有传输arr.id所以报错
-              //   },
-              //   fail: function() {
-              //     cosnole.log("错误");
-              //   }
-              // });
-              // 通过openid 获取改用户的uid  end
-            }
-          }
-        });
-      }
-    })
-  }
 })
